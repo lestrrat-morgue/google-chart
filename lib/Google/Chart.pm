1e8c20e2177b674b706d68e80c195790a9ea1af1
daisuke <daisuke@d0d07461-0603-4401-acd4-de1884942a52>
2008-08-06 04:49:56 +0000

    version++

--- lib/Google/Chart.pm
+++ lib/Google/Chart.pm
@@ -19,7 +19,7 @@ our $USER_AGENT;
 
 use constant BASE_URI => URI->new("http://chart.apis.google.com/chart");
 
-our $VERSION   = '0.05000';
+our $VERSION   = '0.05001';
 our $AUTHORITY = 'cpan:DMAKI';
 
 my %COMPONENTS = (
