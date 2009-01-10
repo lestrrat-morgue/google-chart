37e04f9f43147cad3b1778b1257e7f1794aa96d2
daisuke <daisuke@d0d07461-0603-4401-acd4-de1884942a52>
2009-01-10 12:16:27 +0000

    version++

--- lib/Google/Chart.pm
+++ lib/Google/Chart.pm
@@ -20,7 +20,7 @@ use overload
 
 use constant BASE_URI => URI->new("http://chart.apis.google.com/chart");
 
-our $VERSION   = '0.05011';
+our $VERSION   = '0.05012';
 our $AUTHORITY = 'cpan:DMAKI';
 
 my %COMPONENTS = (
