6137ab16433d352d3aa8f4ca56ca8c1c89974365
lestrrat <lestrrat+github@gmail.com>
2009-10-28 11:55:13 +0900

    put the marker related stuff in one place

--- lib/Google/Chart/WithMarker.pm
+++ lib/Google/Chart/WithMarker.pm
@@ -1,13 +1,12 @@
 package Google::Chart::WithMarker;
 use Moose::Role;
-use Google::Chart::Data::WithMarker;
 use namespace::clean -except => qw(meta);
 
-# We apply a marker trait
-around _build_data => sub {
+around _build_data_traits => sub {
     my ($next, $self) = @_;
-    my $data = $next->($self);
-    Google::Chart::Data::WithMarker->meta->apply( $data );
+    my $traits = $next->($self);
+    push @$traits, 'Google::Chart::Data::WithMarker';
+    return $traits;
 };
 
 around prepare_query => sub {
@@ -43,5 +42,54 @@ around prepare_query => sub {
 
     return @query;
 };
+
+package  # hide from PAUSE
+    Google::Chart::Data::WithMarker;
+use Moose::Role;
+use namespace::clean -except => qw(meta);
+
+around _build_dataset_traits => sub {
+    my ($next, $self) = @_;
+
+    my $traits = $next->($self);
+    push @$traits, 'Google::Chart::DataSet::WithMarker';
+    return $traits;
+};
+
+package # hide from PAUSE
+    Google::Chart::DataSet::WithMarker;
+use Moose::Role;
+use Moose::Util::TypeConstraints;
+use namespace::clean -except => qw(meta);
+
+has marker_type => (
+    is => 'ro',
+    isa => enum([ qw(a c d o s t v V h x) ]),
+    predicate => 'has_marker_type',
+);
+
+has marker_color => (
+    is => 'ro',
+    isa => 'Str',
+    predicate => 'has_marker_color',
+);
+
+has marker_size => (
+    is => 'ro',
+    isa => 'Int',
+    predicate => 'has_marker_size',
+);
+
+has marker_points => (
+    is => 'ro',
+    isa => 'ArrayRef',
+    predicate => 'has_marker_points',
+);
+
+has marker_priority => (
+    is => 'ro',
+    isa => 'Int',
+    predicate => 'has_marker_priority'
+);
 
 1;
