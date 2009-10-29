eb05d6ad64f99b6562f1409af244fa1b0e9d607d
lestrrat <lestrrat+github@gmail.com>
2009-10-29 11:16:31 +0900

    Add map type

---
+++ lib/Google/Chart/Type/Map.pm
@@ -0,0 +1,52 @@
+package Google::Chart::Type::Map;
+use Moose;
+use Moose::Util::TypeConstraints;
+use namespace::clean -except => qw(meta);
+
+extends 'Google::Chart';
+
+with qw(
+    Google::Chart::WithData
+    Google::Chart::WithSolidFill
+);
+
+has area => (
+    is => 'ro',
+    isa => enum([ qw(africa asia europe middle_east south_america usa world) ]),
+    required => 1,
+    default => 'world',
+);
+
+has codes => (
+    is => 'ro',
+    isa => 'ArrayRef',
+    predicate => 'has_codes',
+);
+
+has colors => (
+    is => 'ro',
+    isa => 'ArrayRef',
+    predicate => 'has_colors',
+);
+
+sub _build_type { 't' }
+
+around prepare_query => sub {
+    my ($next, $self, @args) = @_;
+
+    my @query = $next->($self, @args);
+
+    push @query, (chtm => $self->area);
+    if ($self->has_colors) {
+        push @query, (chco => join(',', @{ $self->colors } ));
+    }
+    if ($self->has_codes) {
+        push @query, (chld => join('', @{ $self->codes } ));
+    }
+
+    return @query;
+};
+
+__PACKAGE__->meta->make_immutable();
+
+1;
