97aa623fd35c87380983dd64e533c43aa5ab0750
lestrrat <lestrrat+github@gmail.com>
2009-11-09 19:33:46 +0900

    version++

--- lib/Google/Chart.pm
+++ lib/Google/Chart.pm
@@ -7,7 +7,7 @@ use Google::Chart::Types;
 use LWP::UserAgent;
 use namespace::clean -except => qw(meta);
 
-our $VERSION = '0.09000_03';
+our $VERSION = '0.09000_04';
 
 has title => (
     is        => 'ro',
